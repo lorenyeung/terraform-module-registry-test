d1df59ef3f58bedddcae2bb737a87af6f7eb3d97
Loren Yeung <lorenyeung@users.noreply.github.com>
2025-08-22 08:52:49 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "test-module-registry" {
-  source  = "app.harness.io/WqS38aeyQjayoqy6mzwceA/test-module-registry-another/harness"
+  source  = "loren.harness.io/WqS38aeyQjayoqy6mzwceA/test-module-registry-another/harness"
   version = "v1.0.9"
 
   num_resources = 3
 }
 
 module "karpenter" {
-  source  = "app.harness.io/WqS38aeyQjayoqy6mzwceA/eks/aws//modules/karpenter"
+  source  = "loren.harness.io/WqS38aeyQjayoqy6mzwceA/eks/aws//modules/karpenter"
 }
