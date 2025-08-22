ee04eeccb5a7dac039ab35406aa803e2d1fd53d8
Loren Yeung <lorenyeung@users.noreply.github.com>
2025-08-22 09:26:12 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
 variable "harness_key" {
   description = "Harness API Key"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
 }
