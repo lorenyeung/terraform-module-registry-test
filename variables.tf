5d50bbf9b4dc35452c40fc38053be3709bcb1599
Loren Yeung <lorenyeung@users.noreply.github.com>
2025-08-22 09:40:33 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,16 @@ variable "region" {
   description = "AWS region to deploy resources"
   type        = string
 }
+
+variable "cluster_name" {
+  description = "Name of the existing EKS cluster"
+  type        = string
+}
+
+data "aws_eks_cluster" "eks" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = var.cluster_name
+}
